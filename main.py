10d49d05e8b7a509dbdd48bf69d7d30875bd7f4a
agoupy <alexandre.goupy@gmail.com>
2018-06-19 11:55:49 +0200

    Improve svd and add the mean to the reconstruction

--- main.py
+++ main.py
@@ -39,38 +39,27 @@ data_V=data_V-np.transpose(mtlb.repmat(data_V_mean,data_N,1))
 U, s, V = np.linalg.svd(data_U, full_matrices=True)
 U2, s2, V2 = np.linalg.svd(data_V, full_matrices=True)
 
-# Use the eigenfunctions to reconstruct the profile indref
-ind_ref=784
-ref_U=data_U[:,ind_ref]
-U_4=data_U_mean
-U_8=data_U_mean
-U_16=data_U_mean
-U_32=data_U_mean
-#U_128=data_U_mean
-for order in np.arange(1,4):
-	U_4=U_4+V[order,ind_ref]*s[order]*U[:,order]
-for order in np.arange(1,8):
-        U_8=U_8+V[order,ind_ref]*s[order]*U[:,order]
-for order in np.arange(1,16):
-        U_16=U_16+V[order,ind_ref]*s[order]*U[:,order]
-for order in np.arange(1,32):
-        U_32=U_32+V[order,ind_ref]*s[order]*U[:,order]
-#for order in np.arange(1,128):
-#        U_128=U_128+V[order,ind_ref]*s[order]*U[:,order]
+# Partial reconstruction of the matrix
+U_4=reconst_matrix = np.dot(U[:,:4],np.dot(np.diag(s[:4]),V[:4,:]))
+U_8=reconst_matrix = np.dot(U[:,:8],np.dot(np.diag(s[:8]),V[:8,:]))
+U_16=reconst_matrix = np.dot(U[:,:16],np.dot(np.diag(s[:16]),V[:16,:]))
 
-ref_V=data_V[:,ind_ref]
-V_4=data_V_mean
-V_8=data_V_mean
-V_16=data_V_mean
-V_32=data_V_mean
-for order in np.arange(1,4):
-        V_4=V_4+V2[order,ind_ref]*s2[order]*U2[:,order]
-for order in np.arange(1,8):
-        V_8=V_8+V2[order,ind_ref]*s2[order]*U2[:,order]
-for order in np.arange(1,16):
-        V_16=V_16+V2[order,ind_ref]*s2[order]*U2[:,order]
-for order in np.arange(1,32):
-        V_32=V_32+V2[order,ind_ref]*s2[order]*U2[:,order]
+V_4=reconst_matrix = np.dot(U2[:,:4],np.dot(np.diag(s2[:4]),V2[:4,:]))
+V_8=reconst_matrix = np.dot(U2[:,:8],np.dot(np.diag(s2[:8]),V2[:8,:]))
+V_16=reconst_matrix = np.dot(U2[:,:16],np.dot(np.diag(s2[:16]),V2[:16,:]))
+
+
+# Extract the original and reconstructed profile ind_ref
+ind_ref=784
+ref_U=data_U[:,ind_ref]+data_U_mean
+ref_U_4=U_4[:,ind_ref]+data_U_mean
+ref_U_8=U_8[:,ind_ref]+data_U_mean
+ref_U_16=U_16[:,ind_ref]+data_U_mean
+
+ref_V=data_V[:,ind_ref]+data_V_mean
+ref_V_4=V_4[:,ind_ref]+data_V_mean
+ref_V_8=V_8[:,ind_ref]+data_V_mean
+ref_V_16=V_16[:,ind_ref]+data_V_mean
 
 # Plot the result
 z=np.arange(data_zmin,data_zmax+1)
@@ -82,29 +71,18 @@ plt.subplot(223)
 plt.plot(np.true_divide(np.abs(s2),np.sum(s2)),'b+-')
 plt.title('Meridional EOF weights')
 plt.subplot(222)
-u1,=plt.plot(ref_U,z,'k',Linewidth=2,label='Measured profil')
-u4,=plt.plot(U_4,z,'r',label='4 terms')
-u8,=plt.plot(U_8,z,'y',label='8 terms')
-u16,=plt.plot(U_16,z,'g',label='16 terms')
-u32,=plt.plot(U_32,z,'b',label='32 terms')
-#u128,=plt.plot(U_128,z,'m',label='128 terms')
-plt.legend(handles=[u1, u4, u8, u16, u32])#, u128])
+u1,=plt.plot(ref_U,z,'k',Linewidth=5,label='Measured profil')
+u4,=plt.plot(ref_U_4,z,'r',label='4 terms')
+u8,=plt.plot(ref_U_8,z,'y',label='8 terms')
+u16,=plt.plot(ref_U_16,z,'g',label='16 terms')
+plt.legend(handles=[u1, u4, u8, u16])
 plt.title('Reconstructed Zonal Wind')
 plt.subplot(224)
-v1,=plt.plot(ref_V,z,'k',Linewidth=2,label='Measured profil')
-v4,=plt.plot(V_4,z,'r',label='4 terms')
-v8,=plt.plot(V_8,z,'y',label='8 terms')
-v16,=plt.plot(V_16,z,'g',label='16 terms')
-v32,=plt.plot(V_32,z,'b',label='32 terms')
-plt.legend(handles=[v1, v4, v8, v16, v32])
+v1,=plt.plot(ref_V,z,'k',Linewidth=5,label='Measured profil')
+v4,=plt.plot(ref_V_4,z,'r',label='4 terms')
+v8,=plt.plot(ref_V_8,z,'y',label='8 terms')
+v16,=plt.plot(ref_V_16,z,'g',label='16 terms')
+plt.legend(handles=[v1, v4, v8, v16])
 plt.title('Reconstructed Meridional Wind')
 plt.show()
 
-#plt.axis([-60,60,0,200])
-#plt.title('Zonal wind during the 1/1/2003')
-#z=np.arange(0,201)
-#for i_hour in np.arange(0,23.5,0.5):
-#    hwm14Obj = HWM14( altlim=[0,200], altstp=1, ap=[-1, 35], day=1,
-#            option=1, ut=i_hour, verbose=False, year=2003 )
-#    plt.plot(hwm14Obj.Uwind,z,color=(0.8,0.5,0))    
-#    plt.pause(0.1)
